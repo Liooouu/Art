4f8ed5208caec2289fa887fc671862b2e15171b9
Liooouu <apiadojohnliol@gmail.com>
2025-08-02 22:22:21 +0800

    text test

---
+++ my-artwork/components/ArtPage.jsx
@@ -0,0 +1,16 @@
+export const ArtPage = () => {
+    return (
+        <>
+        <div className="min-h-screen text-center bg-gradient-to-tr from-sky-100 to-blue-700">
+            <h1 className="">
+                Hi
+            </h1>
+            <div className="justify-items-center">
+                <h2>
+                    hello
+                </h2>
+            </div>
+        </div>
+        </>
+    )
+}
--- my-artwork/src/App.jsx
+++ my-artwork/src/App.jsx
@@ -1,13 +1,11 @@
+import { ArtPage } from "../components/ArtPage"
 
 function App() {
 
   return (
-  <div className='min-h-screen w-full bg-gradient-to-b from-indigo-500 to-indigo-900'>
-    <h1>
-      hello
-
-    </h1>
-  </div>
+ <>
+ <ArtPage/>
+ </>
   )
 }
 
