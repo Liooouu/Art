fec44045e7d4baaefe2be2bd5287d6914640dfe4
Liooouu <apiadojohnliol@gmail.com>
2025-08-02 20:42:26 +0800

    fixed

---
+++ my-artwork/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+  <div className='min-h-screen w-full bg-gradient-to-b from-indigo-500 to-indigo-900'>
+    <h1>
+      hello
+
+    </h1>
+  </div>
+  )
+}
+
+export default App
