7530e6a096352e46dda10ab876155564c821b16e
Liooouu <apiadojohnliol@gmail.com>
2025-08-04 15:01:24 +0800

    100% finished

--- my-artwork/Pages/ArtPage.jsx
+++ my-artwork/Pages/ArtPage.jsx
@@ -2,8 +2,26 @@ export const ArtPage = () => {
     return (
         <>    
         <div className="min-h-screen bg-gradient-to-tr from-indigo-400 to-sky-500 flex flex-col items-center justify-center text-center px-4">
+            <h1 className="font-bold font-serif text-3xl py-4">
+                Salvation
+            </h1>
             
-            
+        <div className="mb-6">
+          <img
+            src="/images/Photo2.png"
+            alt="Cover Photo"
+            className="w-full max-w-md rounded shadow-lg"
+          />
+        </div>
+
+        <h2 className="font-semibold font-serif">
+            John 3:16
+        </h2>
+
+        <h2 className="font-serif">
+             For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.
+        </h2>
+
         </div>
         </>
     )
